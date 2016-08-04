fdcf5ac84f173a9e0645f4fbf08e8fc2d34e7ecb
Joe Hoyle <joehoyle@gmail.com>
2016-08-04 16:43:12 +0200

    Add some more logging to what the workers are doing
    The logs right now don't make it that obvious what is coming from the
    Worder and what is coming from the Runner

--- lib/Worker.php
+++ lib/Worker.php
@@ -28,6 +28,7 @@ class Worker {
 		}
 
 		$this->status = proc_get_status( $this->process );
+		printf( '[%d] Worker status: %s' . PHP_EOL, $this->job->id, print_r( $this->status, true ) );
 		return ! ( $this->status['running'] );
 	}
 
@@ -37,6 +38,8 @@ class Worker {
 	 * @return bool Did the process run successfully?
 	 */
 	public function shutdown() {
+
+		printf( '[%d] Worker shutting down...' . PHP_EOL, $this->job->id );
 		// Exhaust the streams
 		while ( ! feof( $this->pipes[1] ) ) {
 			$this->output .= fread( $this->pipes[1], 1024 );
@@ -48,9 +51,9 @@ class Worker {
 		fclose( $this->pipes[1] );
 		fclose( $this->pipes[2] );
 
-		printf( '[%d] out: %s' . PHP_EOL, $this->job->id, $this->output );
-		printf( '[%d] err: %s' . PHP_EOL, $this->job->id, $this->error_output );
-		printf( '[%d] ret: %d' . PHP_EOL, $this->job->id, $this->status['exitcode'] );
+		printf( '[%d] Worker out: %s' . PHP_EOL, $this->job->id, $this->output );
+		printf( '[%d] Worker err: %s' . PHP_EOL, $this->job->id, $this->error_output );
+		printf( '[%d] Worker ret: %d' . PHP_EOL, $this->job->id, $this->status['exitcode'] );
 
 		// Close the process down too
 		proc_close( $this->process );
